01750ac2f56b4a39b94f52dc598a3cb8443271f7
Pravin Varpe <pravinvarpe14@gmail.com>
2022-08-01 07:59:17 +0530

    added the start position programme of snake and ladder of UC1

--- src/Snake_ladder.java
+++ src/Snake_ladder.java
@@ -1,9 +1,14 @@
-
+import java.util.Random;
 public class Snake_ladder {
 
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to snake and ladder game...");
+		
+		int n = 0;
+        Random ran = new Random();
+        n = ran.nextInt(7);
+        System.out.println("The player got the position " + n);
 
 	}
 
